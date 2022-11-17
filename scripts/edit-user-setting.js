2ba762d5bf212d666b0e0cb4d527446fa2f8ecc8
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2022-11-17 13:59:38 +0700

    add

--- scripts/edit-user-setting.js
+++ scripts/edit-user-setting.js
@@ -1,10 +1,17 @@
 const submitAdmin = () => {
   let firstName = $("#first-name").val();
   let lastName = $("#last-name").val();
-  let username = $("#usernme").val();
+  let position = $("#position").val();
+  let username = $("#username").val();
   let password = $("#password").val();
 
-  if (firstName == "" || lastName == "" || username == "" || password == "") {
+  if (
+    firstName == "" ||
+    lastName == "" ||
+    position == "" ||
+    username == "" ||
+    password == ""
+  ) {
     dialog.showMessageBoxSync({
       type: "info",
       title: "Alert",
@@ -18,7 +25,7 @@ const submitAdmin = () => {
 
         if (result.length < 1) {
           db.run(
-            `insert into user (first_name, last_name, username, password, access_level) values ('${firstName}', '${lastName}', '${username}', '${password}', 'admin')`,
+            `insert into user (first_name, last_name, position, username, password, access_level) values ('${firstName}', '${lastName}', '${position}', '${username}', '${password}', 'admin')`,
             (err) => {
               if (err) throw err;
               ipcRenderer.send("success:update-user");
@@ -28,6 +35,69 @@ const submitAdmin = () => {
           dialog.showErrorBox(
             "User already existed",
             "Username sudah terdaftar"
+          );
+        }
+      }
+    );
+  }
+};
+
+const submitEditUsername = (username, id) => {
+  let newUsername = $("#new-username").val();
+
+  if (newUsername == "") {
+    dialog.showMessageBoxSync({
+      type: "info",
+      title: "Alert",
+      message: "Username baru harus diisi",
+    });
+  } else if (newUsername == username) {
+    ipcRenderer.send("success:update-user");
+  } else {
+    db.all(
+      `select * from user where username = '${newUsername}'`,
+      (err, rows) => {
+        if (err) throw err;
+        if (rows.length < 1) {
+          db.run(
+            `update user set username = '${newUsername}' where id = ${id}`,
+            (err) => {
+              if (err) throw err;
+              ipcRenderer.send("success:update-user");
+            }
+          );
+        } else {
+          dialog.showErrorBox("Username exist", "Username sudah terdaftar");
+        }
+      }
+    );
+  }
+};
+
+const submitEditPassword = (id) => {
+  let prevPassword = $("#prev-password").val();
+  let newPassword = $("#new-password").val();
+
+  if (prevPassword == "" || newPassword == "") {
+    dialog.showMessageBoxSync({
+      type: "info",
+      title: "Alert",
+      message: "Password lama dan baru harus diisi",
+    });
+  } else {
+    db.all(
+      `select * from user where id = ${id} and password = '${prevPassword}'`,
+      (err, rows) => {
+        if (err) throw err;
+        if (rows.length < 1) {
+          dialog.showErrorBox("Invalid password", "Password salah");
+        } else {
+          db.run(
+            `update user set password = '${newPassword}' where id = ${id}`,
+            (err) => {
+              if (err) throw err;
+              ipcRenderer.send("success:update-user");
+            }
           );
         }
       }
